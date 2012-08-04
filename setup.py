a4da5c705b47a8089f04a81bec11fe673f6565a2
Derek Stegelman <dstegelman@gmail.com>
2012-08-03 21:12:53 -0500

    Add package.

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+version = 'dev'
+
+setup(name='django-downtime',
+      version=version,
+      description="Give your site a down page, or redirect to an offline error page.",
+      long_description=open("README.md", "r").read(),
+      classifiers=[
+                   "Development Status :: 5 - Production/Stable",
+                   "Environment :: Web Environment",
+                   "Intended Audience :: End Users/Desktop",
+                   "Natural Language :: English",
+                   "Operating System :: OS Independent",
+                   "Framework :: Django",
+                   "Programming Language :: Python",
+                   "Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries",
+                   "Topic :: Utilities",
+                   "License :: OSI Approved :: MIT License",
+                   ],
+      keywords='',
+      author='Derek Stegelman',
+      author_email='dstegelman@gmail.com',
+      url='http://github.com/dstegelman/django-downtime',
+      license='MIT',
+      packages=find_packages(),
+      install_requires = [],
+      include_package_data=True,
+      zip_safe=False,
+      )
