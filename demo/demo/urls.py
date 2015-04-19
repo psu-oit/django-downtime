e800bbb4803ccaa3a56a834b8e422deaab96ae3b
Derek Stegelman <dstegelman@gmail.com>
2015-04-19 15:05:10 -0600

    Add cases to demo.

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^hello/$', TemplateView.as_view(template_name='hello.html')),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', name='auth_login'),
 
 )
